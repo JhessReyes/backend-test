276c90446bbb1a31bf1f39b416477c0f15cd4948
JhessReyes <jhessaureyes@gmail.com>
2025-04-10 00:54:23 -0600

    fix xml

--- src/main/kotlin/stsa/kotlin_htmx/api/v1/utils/XmlUtils.kt
+++ src/main/kotlin/stsa/kotlin_htmx/api/v1/utils/XmlUtils.kt
@@ -3,6 +3,16 @@ package stsa.kotlin_htmx.api.v1.utils
 import stsa.kotlin_htmx.api.v1.responses.externalSource.SkinResponse
 
 object XmlUtils {
+    private fun escapeHtmlTags(input: String): String {
+        return input
+            .replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;")
+            .replace("'", "&apos;")
+    }
+
+
     fun skinsToXml(skins: List<SkinResponse>): String {
         val xml: String
         if (skins.isNotEmpty()) {
@@ -12,23 +22,27 @@ object XmlUtils {
                 skins.forEach { s ->
                     append("<skin>")
                     append("<id>${s.id}</id>")
-                    /*append("<name>${s.name}</name>")*/
-                    /* append("<description>${s.description}</description>")*/
-                    /* append("<image>${s.image}</image>")*/
-                    /* append("<team>")
-                     append("<id>${s.team.id}</id>")
-                     append("<name>${s.team.name}</name>")
-                     append("</team>")*/
-                    /*s.crates.forEach { c ->
-                        append("<crate>")
-                        append("<id>${c.id}</id>")*/
-                    /*append("<name>${c.name}</name>")*/
-                    /* if (!c.description.isNullOrEmpty()) {
-                         append("<description>${c.description}</description>")
-                     }*/
-                    /* append("<image>${c.image}</image>")*/
-                    /*  append("</crate>")
-                  }*/
+                    append("<name>${escapeHtmlTags(s.name)}</name>")
+                    append("<description>${escapeHtmlTags(s.description)}</description>")
+                    append("<image>${s.image}</image>")
+                    append("<team>")
+                    append("<id>${s.team.id}</id>")
+                    append("<name>${s.team.name}</name>")
+                    append("</team>")
+                    if (s.crates.isNotEmpty()) {
+                        append("<crates>")
+                        s.crates.forEach { c ->
+                            append("<crate>")
+                            append("<id>${c.id}</id>")
+                            append("<name>${escapeHtmlTags(c.name)}</name>")
+                            if (!c.description.isNullOrEmpty()) {
+                                append("<description>${escapeHtmlTags(c.description)}</description>")
+                            }
+                            append("<image>${c.image}</image>")
+                            append("</crate>")
+                        }
+                        append("</crates>")
+                    }
                     append("</skin>")
                 }
                 append("</skins>")
