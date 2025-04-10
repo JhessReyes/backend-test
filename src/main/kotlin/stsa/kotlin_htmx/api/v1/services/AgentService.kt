dc827fddc45a7a9eb56ac25721a5414390ae7240
JhessReyes <jhessaureyes@gmail.com>
2025-04-10 02:45:54 -0600

    cache implement agents

--- src/main/kotlin/stsa/kotlin_htmx/api/v1/services/AgentService.kt
+++ src/main/kotlin/stsa/kotlin_htmx/api/v1/services/AgentService.kt
@@ -2,13 +2,34 @@ package stsa.kotlin_htmx.api.v1.services
 
 import stsa.kotlin_htmx.api.v1.responses.externalSource.AgentResponse
 import stsa.kotlin_htmx.api.v1.responses.externalSource.TeamResponse
+import stsa.kotlin_htmx.api.v1.utils.CacheManager
+import stsa.kotlin_htmx.database.models.AgentModel
 import stsa.kotlin_htmx.database.repositories.AgentRepository
 import stsa.kotlin_htmx.database.repositories.WhereAgent
+import java.util.concurrent.TimeUnit
 
 class AgentService(private val repository: AgentRepository) {
+    private val agentCache = CacheManager<String, List<AgentModel>>(
+        ttlMillis = TimeUnit.MINUTES.toMillis(10)
+    )
+
     suspend fun getAgentsOnWhere(whereAgent: WhereAgent): List<AgentResponse> {
-        val agents = repository.getAgents(whereAgent)
-        return agents.map { i ->
+        val filters = mapOf(
+            "id" to whereAgent.id,
+            "name" to whereAgent.name,
+            "description" to whereAgent.description,
+            "image" to whereAgent.image,
+            "teamName" to whereAgent.teamName
+        )
+        val key = agentCache.generateKey("agents", filters = filters)
+
+        val cachedAgents = agentCache.get(key) ?: run {
+            val crates = repository.getAgents(whereAgent)
+            agentCache.put(key, crates)
+            crates
+        }
+
+        return cachedAgents.map { i ->
             AgentResponse(
                 id = i.id,
                 name = i.name,
